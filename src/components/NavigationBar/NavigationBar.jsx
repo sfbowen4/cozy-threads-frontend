a2591f76e0e09fe881d76056a0b5c3bef8b05762
Stephen Bowen <sbowen@architectnow.net>
2025-01-28 00:00:53 -0600

    Hammer down structure and begin component styling/placement

---
+++ src/components/NavigationBar/NavigationBar.jsx
@@ -0,0 +1,11 @@
+import "./NavigationBar.css";
+
+export default function NavigationBar() {
+    return (
+        <div className="nav-bar">
+            <a href="#home">Home</a>
+            <a href="#shop">Shop</a>
+            <a href="#about">About</a>
+        </div>
+    );
+}
---
+++ src/pages/LandingPage/LandingPage.jsx
@@ -0,0 +1,8 @@
+export default function LandingPage() {
+  return (
+    <div>
+      <h1>Welcome to Cozy Threads</h1>
+      <p>Discover the latest in cozy fashion</p>
+    </div>
+  );
+}
